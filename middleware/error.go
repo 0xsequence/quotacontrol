8efd5c8087af1a775897ea6f0e19401dbd3e1804
Peter Kieltyka <peter.kieltyka@gmail.com>
2023-11-20 14:26:52 -0500

    continue in case of failure

--- middleware/error.go
+++ middleware/error.go
@@ -17,17 +17,25 @@ func FailOnUnexpectedError(fn func(w http.ResponseWriter, err error)) func(w htt
 
 func ContinueOnUnexpectedError(log logger.Logger, fn func(w http.ResponseWriter, err error)) func(w http.ResponseWriter, _ *http.Request, next http.Handler, err error) {
 	return func(w http.ResponseWriter, r *http.Request, next http.Handler, err error) {
+		log.With("err", err, "op", "quota").Error("-> quotacontrol: unexpected error")
 		if !shouldErrorContinue(log, err) {
 			fn(w, err)
 			return
 		}
-		log.With("err", err, "op", "quota").Error("-> quotacontrol: unexpected error")
+		next.ServeHTTP(w, r.WithContext(WithSkipRateLimit(r.Context())))
+	}
+}
+
+func ContinueOnAnyError(log logger.Logger, fn func(w http.ResponseWriter, err error)) func(w http.ResponseWriter, _ *http.Request, next http.Handler, err error) {
+	return func(w http.ResponseWriter, r *http.Request, next http.Handler, err error) {
+		log.With("err", err, "op", "quota").Error("-> quotacontrol: unexpected error -- continuing anyways")
 		next.ServeHTTP(w, r.WithContext(WithSkipRateLimit(r.Context())))
 	}
 }
 
 func shouldErrorContinue(log logger.Logger, err error) bool {
 	w := proto.WebRPCError{}
+
 	// Unexpected error
 	if !errors.As(err, &w) {
 		// Sample log of unexpected errors (every 10 seconds)
@@ -36,13 +44,21 @@ func shouldErrorContinue(log logger.Logger, err error) bool {
 		}
 		return true
 	}
+
 	// QuotaControl server down
 	if errors.Is(w, proto.ErrWebrpcRequestFailed) {
 		return true
 	}
+
 	// QuotaControl method not found
 	if errors.Is(w, proto.ErrWebrpcBadRoute) {
 		return true
 	}
+
+	// QuotaControl timed out
+	if errors.Is(w, proto.ErrTimeout) {
+		return true
+	}
+
 	return false
 }
