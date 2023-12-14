460397737abbefe8f1f8f61ab3213e604c6eb8c6
Alex Guerrieri <ag@horizon.io>
2023-12-14 11:35:09 +0100

    default rate detector

--- config.go
+++ config.go
@@ -1,9 +1,12 @@
 package quotacontrol
 
 import (
+	"net/http"
+	"strings"
 	"time"
 
 	httprateredis "github.com/go-chi/httprate-redis"
+	"github.com/go-chi/jwtauth/v5"
 	"github.com/goware/cachestore/redis"
 )
 
@@ -39,9 +42,31 @@ func (cfg Config) RedisRateLimitConfig() *httprateredis.Config {
 }
 
 type RateLimiterConfig struct {
-	Enabled                  bool   `toml:"enabled"`
-	PublicRequestsPerMinute  int    `toml:"public_requests_per_minute"`
-	UserRequestsPerMinute    int    `toml:"user_requests_per_minute"`
-	ServiceRequestsPerMinute int    `toml:"service_requests_per_minute"`
-	ErrorMessage             string `toml:"error_message"`
+	Enabled      bool         `toml:"enabled"`
+	ErrorMessage string       `toml:"error_message"`
+	DefaultRates DefaultRates `toml:"default_rates"`
+}
+
+type DefaultRates struct {
+	Public  int `toml:"public"`
+	User    int `toml:"user"`
+	Service int `toml:"service"`
+}
+
+// DetectRate retuns key and limit for rate limiter when access key si not available.
+func (d DefaultRates) DetectRate(r *http.Request) (key string, limit int) {
+	_, claims, err := jwtauth.FromContext(r.Context())
+	if err != nil || claims == nil {
+		return "", d.Public
+	}
+
+	if svc, _ := claims["service"].(string); svc != "" {
+		return "service:" + svc, d.Service
+	}
+
+	if account, _ := claims["account"].(string); account != "" {
+		return "user:" + strings.ToLower(account), d.User
+	}
+
+	return "", d.Public
 }
