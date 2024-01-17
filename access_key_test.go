8659e7fae5ff6e8a9b13c68946bd6c3390d0db88
Peter Kieltyka <peter.kieltyka@gmail.com>
2024-01-17 11:34:06 -0500

    update

--- access_key_test.go
+++ access_key_test.go
@@ -1,7 +1,6 @@
 package quotacontrol_test
 
 import (
-	"fmt"
 	"testing"
 
 	"github.com/0xsequence/quotacontrol"
@@ -11,7 +10,7 @@ import (
 func TestAccessKeyV1Encoding(t *testing.T) {
 	projectID := uint64(12345)
 	accessKey := quotacontrol.GenerateAccessKey(projectID)
-	fmt.Println("=> k", accessKey)
+	// fmt.Println("=> k", accessKey)
 
 	outID, err := quotacontrol.DecodeProjectID(accessKey)
 	require.NoError(t, err)
@@ -21,7 +20,7 @@ func TestAccessKeyV1Encoding(t *testing.T) {
 func TestAccessKeyLegacyEncoding(t *testing.T) {
 	projectID := uint64(12345)
 	accessKey := quotacontrol.LegacyGenerateAccessKey(projectID)
-	fmt.Println("=> k", accessKey)
+	// fmt.Println("=> k", accessKey)
 
 	outID, err := quotacontrol.LegacyDecodeProjectID(accessKey)
 	require.NoError(t, err)
