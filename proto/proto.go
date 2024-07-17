47df73ae49aebb63652c23c4d79b76a52ff09cd5
Alex Guerrieri <ag@horizon.io>
2024-07-17 12:57:02 +0200

    Add JS client back

--- proto/proto.go
+++ proto/proto.go
@@ -1,4 +1,5 @@
 //go:generate go run github.com/webrpc/webrpc/cmd/webrpc-gen -schema=proto.ridl -target=golang@v0.14.8 -pkg=proto -server -client -out=./proto.gen.go
+//go:generate go run github.com/webrpc/webrpc/cmd/webrpc-gen -schema=proto.ridl -target=typescript@v0.12.0 -client -out=./clients/quotacontrol.gen.ts
 package proto
 
 import (
