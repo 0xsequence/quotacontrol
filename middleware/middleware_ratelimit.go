59973ff468da95e8b7135881e9a91e3e49ca6371
Alex Guerrieri <ag@horizon.io>
2024-10-21 18:00:35 +0200

    nit

--- middleware/middleware_ratelimit.go
+++ middleware/middleware_ratelimit.go
@@ -66,15 +66,15 @@ func RateLimit(rlCfg RLConfig, redisCfg redis.Config, eh ErrHandler) func(next h
 
 	var limitCounter httprate.LimitCounter
 	if redisCfg.Enabled {
-		cfg := httprateredis.Config{
+		cfg := &httprateredis.Config{
 			Host:      redisCfg.Host,
 			Port:      redisCfg.Port,
 			MaxIdle:   redisCfg.MaxIdle,
 			MaxActive: redisCfg.MaxActive,
 			DBIndex:   redisCfg.DBIndex,
 		}
-		if c, err := httprateredis.NewRedisLimitCounter(&cfg); err != nil {
-			slog.Error("redis limit counter", slog.Any("error", err))
+		if c, err := httprateredis.NewRedisLimitCounter(cfg); err != nil {
+			slog.Error("redis counter not available", slog.Any("error", err))
 		} else {
 			limitCounter = c
 		}
