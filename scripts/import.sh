69e5d0f5ef781c86ec92fbb2be5add80ee7c08c7
Alex Guerrieri <ag@horizon.io>
2024-10-31 16:47:07 +0100

    fix import script

--- scripts/import.sh
+++ scripts/import.sh
@@ -12,8 +12,8 @@ TAG=$(cat go.mod | grep -v "//" | grep $PKG | awk '{print$ 2}')
 URL="https://raw.githubusercontent.com/$PKG/refs/tags/$TAG/proto/quotacontrol.ridl"
 
 COMMIT=$(echo $TAG | cut -d'-' -f 3)
-if [ -n "$COMMIT" ]; then
-    URL="https://raw.githubusercontent.com/$PKG/refs/heads/$COMMIT/proto/quotacontrol.ridl"
+if [[ "$TAG" != "$COMMIT" ]]; then
+    URL="https://raw.githubusercontent.com/$PKG/refs/heads/$COMMIT/proto/quotacontrol.ridl";
 fi
 
 printf "# $PKG $TAG \n";
