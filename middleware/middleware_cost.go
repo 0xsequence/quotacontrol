7f757ff3a902cb6520ebbbd880b81815e8d232ba
Alex - アレックス <ag@horizon.io>
2025-01-16 16:29:55 +0100

    No message for missing cost (#80)

--- middleware/middleware_cost.go
+++ middleware/middleware_cost.go
@@ -1,26 +1,22 @@
 package middleware
 
 import (
-	"log/slog"
 	"net/http"
 
 	"github.com/0xsequence/authcontrol"
 )
 
 // SetCost middleware that sets the cost of the request, and defaults to Option.BaseRequestCost.
-func SetCost(cost authcontrol.Config[int64], o Options) func(next http.Handler) http.Handler {
+func SetCost(cfg authcontrol.Config[int64], o Options) func(next http.Handler) http.Handler {
 	o.ApplyDefaults()
 
 	return func(next http.Handler) http.Handler {
 		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 			ctx := r.Context()
 
-			cost, err := cost.Get(ctx, r.URL.Path)
-			if err != nil {
-				if o.Logger != nil {
-					o.Logger.Error("get cost", slog.Any("error", err))
-				}
-				cost = int64(o.BaseRequestCost)
+			cost := int64(o.BaseRequestCost)
+			if v, err := cfg.Get(ctx, r.URL.Path); err == nil {
+				cost = v
 			}
 
 			ctx = WithCost(ctx, cost)
